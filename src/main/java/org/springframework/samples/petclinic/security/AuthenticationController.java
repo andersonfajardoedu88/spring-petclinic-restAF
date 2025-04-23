81707fde6b05b48415f74e19552e3b1f617b4475
andersonfajardoedu88 <129015305+andersonfajardoedu88@users.noreply.github.com>
2025-04-23 23:28:36 +0000

    criado e adicionado o arquivo authenticationController.java que basicamente é um endpoint dos guris em rest, para login e geração dos tokens em JWT

---
+++ src/main/java/org/springframework/samples/petclinic/security/AuthenticationController.java
@@ -0,0 +1,35 @@
+package org.springframework.samples.petclinic.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/authenticate")
+    public String createAuthenticationToken(@RequestBody AuthenticationRequest request) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
+            );
+        } catch (AuthenticationException e) {
+            throw new Exception("Usuário ou senha inválidos", e);
+        }
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
+        return jwtUtil.generateToken(userDetails);
+    }
+}
